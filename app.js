b19f8d57778e670fb311f6598761600680fad233
Ahmed Sherif <ahmed.sherif146@outlook.com>
2021-02-03 01:50:11 +0200

    added basic auth to client

--- app.js
+++ app.js
@@ -55,7 +55,8 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve(__dirname, 'client', 'build')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build'));
+        console.log(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 else {

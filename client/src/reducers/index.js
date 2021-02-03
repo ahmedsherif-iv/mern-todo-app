85d87c71cd7dd8c342707ebe20e44b62eebb8dd1
Ahmed Sherif <ahmed.sherif146@outlook.com>
2021-02-03 20:31:44 +0200

    removed old client

--- client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import userReducer from '../reducers/userReducer';
-
-const rootReducer = combineReducers({
-    userReducer,
-});
-
-export default rootReducer;

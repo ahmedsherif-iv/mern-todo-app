6ff013f78e8ec99d15d1ef36ecebebc5369681fc
ahmedsherif-iv <ahmed.sherif146@outlook.com>
2021-02-05 17:14:08 +0200

    added responsive view for auth form

--- client/src/components/AuthForm/AuthForm.js
+++ client/src/components/AuthForm/AuthForm.js
@@ -1,59 +1,124 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "../Button/Button";
 import "./AuthForm.css"
 
 const AuthForm = () => {
     const [container, setContainer] = useState('');
+    const [mobileView, setMobileView] = useState(false);
+    const [viewSignupForm, setViewSignupForm] = useState(false);
 
     const showSignup = flag => flag ? setContainer('') : setContainer('right-panel-active');
 
+    const toggleAuth = () => setViewSignupForm(prev => !prev);
+
+    const switchTabView = () => {
+        if (window.innerWidth <= 600) {
+            setMobileView(true);
+        } else {
+            setMobileView(false);
+        }
+    };
+
+    useEffect(() => {
+        switchTabView();
+    }, []);
+
+    window.addEventListener('resize', switchTabView);
+
     return (
         <>
-            <div className={`auth-container ${container}`}>
-                <div className="form-container sign-up-container">
-                    <form action="#">
-                        <h1>Create Account</h1>
-                        <div className="social-container">
-                            <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
-                            <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
-                        </div>
-                        <span>or use your email for registration</span>
-                        <input type="text" placeholder="Name" />
-                        <input type="email" placeholder="Email" />
-                        <input type="password" placeholder="Password" />
-                        <button>Sign Up</button>
-                    </form>
+            {mobileView && (
+                <div className="mobile-tabs">
+                    {/* <Button>Sign in</Button>
+                    <Button>Sign up</Button> */}
+                    <button className={!viewSignupForm ? 'selected' : ''} onClick={toggleAuth}>Sign in</button>
+                    <button className={viewSignupForm ? 'selected' : ''} onClick={toggleAuth}>Sign up</button>
                 </div>
-                <div className="form-container sign-in-container">
-                    <form action="#">
-                        <h1>Sign in</h1>
-                        <div className="social-container">
-                            <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
-                            <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
-                        </div>
-                        <span>or use your account</span>
-                        <input type="email" placeholder="Email" />
-                        <input type="password" placeholder="Password" />
-                        <a href="#">Forgot your password?</a>
-                        <button>Sign In</button>
-                    </form>
-                </div>
-                <div className="overlay-container">
-                    <div className="overlay">
-                        <div className="overlay-panel overlay-left">
-                            <h1>Welcome Back!</h1>
-                            <p>To keep connected with us please login with your personal info</p>
-                            {/* <button className="ghost" onClick={() => showSignup(true)}>Sign In</button> */}
-                            <Button buttonClass="ghost" buttonStyle="btn--outline" onClick={() => showSignup(true)}>Sign In</Button>
-                        </div>
-                        <div className="overlay-panel overlay-right">
-                            <h1>Hello, Friend!</h1>
-                            <p>Enter your personal details and start journey with us</p>
-                            <button className="ghost" onClick={() => showSignup(false)}>Sign Up</button>
+            )}
+
+            {!mobileView && (
+                <div className={`auth-container ${container}`}>
+                    <div className="form-container sign-up-container">
+                        <form action="#">
+                            <h1>Create Account</h1>
+                            <div className="social-container">
+                                <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
+                                <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
+                            </div>
+                            <span>or use your email for registration</span>
+                            <input type="text" placeholder="Name" />
+                            <input type="email" placeholder="Email" />
+                            <input type="password" placeholder="Password" />
+                            <button>Sign Up</button>
+                        </form>
+                    </div>
+                    <div className="form-container sign-in-container">
+                        <form action="#">
+                            <h1>Sign in</h1>
+                            <div className="social-container">
+                                <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
+                                <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
+                            </div>
+                            <span>or use your account</span>
+                            <input type="email" placeholder="Email" />
+                            <input type="password" placeholder="Password" />
+                            <a href="#">Forgot your password?</a>
+                            <button>Sign In</button>
+                        </form>
+                    </div>
+                    <div className="overlay-container">
+                        <div className="overlay">
+                            <div className="overlay-panel overlay-left">
+                                <h1>Welcome Back!</h1>
+                                <p>To keep connected with us please login with your personal info</p>
+                                {/* <button className="ghost" onClick={() => showSignup(true)}>Sign In</button> */}
+                                <Button buttonClass="ghost" buttonStyle="btn--outline" onClick={() => showSignup(true)}>Sign In</Button>
+                            </div>
+                            <div className="overlay-panel overlay-right">
+                                <h1>Hello, Friend!</h1>
+                                <p>Enter your personal details and start journey with us</p>
+                                <button className="ghost" onClick={() => showSignup(false)}>Sign Up</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            )}
+
+            {mobileView && (
+                <div className='auth-container'>
+                    <div className="form-container sign-in-container">
+                        {!viewSignupForm && (
+                            <form action="#">
+                                <h1>Sign in</h1>
+                                <div className="social-container">
+                                    <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
+                                    <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
+                                </div>
+                                <span>or use your account</span>
+                                <input type="email" placeholder="Email" />
+                                <input type="password" placeholder="Password" />
+                                <a href="#">Forgot your password?</a>
+                                <button>Sign In</button>
+                            </form>
+                        )}
+                        {viewSignupForm && (
+                            <form action="#">
+                                <h1>Create Account</h1>
+                                <div className="social-container">
+                                    <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
+                                    <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
+                                </div>
+                                <span>or use your email for registration</span>
+                                <input type="text" placeholder="Name" />
+                                <input type="email" placeholder="Email" />
+                                <input type="password" placeholder="Password" />
+                                <button>Sign Up</button>
+                            </form>
+                        )}
+                    </div>
+                </div>
+            )
+            }
         </>
     );
 }

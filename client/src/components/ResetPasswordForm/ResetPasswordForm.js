b8862bc3fdafe87ff222cb5e8fdc2f598aac4485
ahmedsherif-iv <ahmed.sherif146@outlook.com>
2021-02-06 04:08:25 +0200

    added reset password form to client

--- client/src/components/ResetPasswordForm/ResetPasswordForm.js
+++ client/src/components/ResetPasswordForm/ResetPasswordForm.js
@@ -1,6 +1,19 @@
+import './ResetPasswordForm.css';
+import { Button } from "../Button/Button";
+
 const ResetPasswordForm = () => {
     return (
         <>
+            <section className='reset-password-section'>
+                <div className='reset-password-container'>
+                    <h1>Having trouble signing in?</h1>
+                    <p>Enter your Email.</p>
+                    <form action="#">
+                        <input type="email" placeholder="Enter your email" />
+                    </form>
+                    <button type="submit">continue</button>
+                </div>
+            </section>
         </>
     );
 }
--- client/src/pages/ResetPasswordPage.js
+++ client/src/pages/ResetPasswordPage.js
@@ -1,10 +1,12 @@
 import Footer from "../components/Footer/Footer"
 import Navbar from "../components/Navbar/Navbar";
+import ResetPasswordForm from "../components/ResetPasswordForm/ResetPasswordForm";
 
 const ResetPassword = () => {
     return (
         <>
             <Navbar />
+            <ResetPasswordForm />
             <Footer />
         </>
     );

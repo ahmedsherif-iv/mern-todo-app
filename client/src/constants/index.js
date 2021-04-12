0a4351a410815f11f89a95b4d9a9efb83c5e8f68
Ahmed Sherif <ahmed.sherif146@outlook.com>
2021-04-12 14:34:07 +0200

    Delete client directory

--- client/src/constants/index.js
+++
@@ -1,3 +0,0 @@
-export const GOOGLE_AUTH_URL = 'http://localhost:5000/api/auth/google';
-export const FACEBOOK_AUTH_URL = 'http://localhost:5000/api/auth/facebook';
-export const SOCKET_ENDPOINT = 'http://localhost:5000';
